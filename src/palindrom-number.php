d3bbe7d216ad13e7176599552a850559941c0af6
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2021-04-29 17:40:20 +0200

    finished palindrom-number

--- src/palindrom-number.php
+++ src/palindrom-number.php
@@ -1,9 +1,33 @@
 <?php
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ *
+Example 1:
+
+Input: x = 121
+Output: true
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Example 4:
+
+Input: x = -101
+Output: false
+ */
+
+$solution = new Solution();
+echo $solution->isPalindrome();
 
 /**
  * Class Solution
  */
-
 class Solution {
 
     /**
@@ -15,17 +39,14 @@ class Solution {
         $nums = str_split($x);
         $len = count($nums);
         if ($len == 1) return true;
-        if ($len%2 == 0) {
-            $pos_r = (int)$len/2;
-            $pos_l = $pos_r - 1;
-            for($pos_l; $pos_l <= 0; $pos_l--) {
-                if ($pos_l != $pos_r) {
-                    return false;
-                }
-                $pos_r++;
+        $middle = (int)$len/2;
+        $pos_r = ($len%2 == 0) ? $middle : $middle + 1;
+        $pos_l = $middle - 1;
+        for($pos_l; $pos_l >= 0; $pos_l--) {
+            if ($nums[$pos_l] != $nums[$pos_r]) {
+                return false;
             }
-        } else {
-
+            $pos_r++;
         }
         return true;
     }
